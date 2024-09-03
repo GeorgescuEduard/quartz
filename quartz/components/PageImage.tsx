b9f2a4ab8608f9218c047bc87e5a56624a6abaf4
Eduard <georgescu.ioneduard@gmail.com>
2024-09-03 16:40:05 +0300

    Replaced the image

--- quartz/components/PageImage.tsx
+++ quartz/components/PageImage.tsx
@@ -6,7 +6,7 @@ import { i18n } from "../i18n"
 const PageImage: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <img src="https://www.adobe.com/products/firefly/features/media_1cc035824a5dbf414f18a55ef622e86cc51684dd5.png?width=750&format=png&optimize=medium"></img>
+    <img id="profile-picture" src="profile-picture-modified.png"></img>
   )
 }
 
