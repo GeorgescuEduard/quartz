c8f92c3807cd094114d41a40e2c72ac2d21d4e1f
Eduard <georgescu.eduard@outlook.com>
2024-09-15 18:42:09 +0300

    Trying another baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ion-eduard-georgescu.vercel.app",
+    baseUrl: "github.com/GeorgescuEduard/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
