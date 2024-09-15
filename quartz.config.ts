794d516a936c6f50c95d83ed1442f344e01c3c93
Eduard <georgescu.eduard@outlook.com>
2024-09-15 18:26:10 +0300

    Updated config for routing

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ion-eduard-georgescu.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
