c676e921f11da0e3bbff0889f54fd06f5598a26c
Eduard <georgescu.ioneduard@gmail.com>
2024-09-17 09:23:05 +0300

    Resolved responsive issues with profile picture

--- quartz/components/PageImage.tsx
+++ quartz/components/PageImage.tsx
@@ -6,13 +6,16 @@ import { i18n } from "../i18n"
 const PageImage: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <img id="profile-picture" src="/images/profile-picture.jpg" className={"page-image"}></img>
+    <div class={classNames(displayClass, "page-image")}>
+      <img id="profile-picture" src="/images/profile-picture.jpg" className={"page-image"}></img>
+    </div>
   )
 }
 
 PageImage.css = `
 .page-image {
   width:85%;
+  max-width:250px;
 }
 `
 
